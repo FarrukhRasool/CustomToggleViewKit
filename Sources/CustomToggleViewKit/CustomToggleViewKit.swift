ee243090a2f485dda16b02ed9f684a44033ca043
FarrukhRasool <farrukh.rasool@pakeventures.com>
2025-04-23 13:46:17 +0200

    custom toggle package ready to ship

--- Sources/CustomToggleViewKit/CustomToggleViewKit.swift
+++ Sources/CustomToggleViewKit/CustomToggleViewKit.swift
@@ -1,2 +1,45 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+struct CustomToggleViewKit: View {
+    let options = ["1", "2", "3", "4", "5"]
+    @State private var selectedOption: Int? = nil
+    var body: some View {
+        HStack(alignment: .top){
+            HStack {
+                Image(systemName: "square.and.pencil.circle.fill")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                
+            }
+            VStack {
+                HStack {
+                    Text("Kindly select you priority")
+                    Spacer()
+                }
+                HStack(spacing: 12) {
+                    ForEach(0..<options.count, id: \.self) { index in
+                        
+                        Toggle(options[index], isOn: Binding(
+                            get: { selectedOption == index },
+                            set: { newValue in
+                                if newValue {
+                                    selectedOption = index
+                                } else {
+                                    selectedOption = nil
+                                }
+                            }
+                        ))
+                        .toggleStyle(CircularCheckBoxStyle(value: options[index]))
+                    }
+                    Spacer()
+                }
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    CustomToggleViewKit()
+}
+
